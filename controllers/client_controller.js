5ab66bc3a4e9dd22be114f05e4fbc8a780f64973
nicoleonard <nikolasleonard@gmail.com>
2023-02-13 01:04:09 -0300

    Añadida la funcion de eliminar clientes del servidor. Ejemplo de uso de verbos http POST y DELETE.

--- controllers/client_controller.js
+++ controllers/client_controller.js
@@ -1,6 +1,6 @@
 import { clientServices } from "../service/client_service.js";
 
-const crearNuevaLinea = (nombre,email) => {
+const crearNuevaLinea = (nombre,email, id) => {
     const linea = document.createElement("tr")
     const contenido =`
     <td class="td" data-td>${nombre}</td>
@@ -17,7 +17,7 @@ const crearNuevaLinea = (nombre,email) => {
         <li>
           <button
             class="simple-button simple-button--delete"
-            type="button"
+            type="button" id="${id}"
           >
             Eliminar
           </button>
@@ -25,14 +25,22 @@ const crearNuevaLinea = (nombre,email) => {
       </ul>
     </td>`
     linea.innerHTML = contenido;
+    const btn = linea.querySelector("button");
+    btn.addEventListener("click", () => {
+        const id = btn.id;
+        clientServices.eliminarCliente(id).then((respuesta) => {
+            console.log(`Se elimino al cliente ${id}`);
+        }).catch(error => alert("Ocurrio un error"));
+    })
     return linea;
 };
 
 const table = document.querySelector("[data-table]")
 
 clientServices.listaClientes().then((data) => {
-    data.forEach( perfil => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
+    data.forEach( ({ nombre, email, id }) => {
+        const nuevaLinea = crearNuevaLinea(nombre, email, id)
         table.appendChild(nuevaLinea);
     });
 }).catch((error) => alert("Ocurrió un error"));
+
